969aaf2c1c3c01ea11ed5a484aff5ca601b54b48
pfmao <ErenJagerM@outlook.com>
2021-09-22 15:16:31 +0800

    feat: add string

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,21 @@ class Eva {
             return exp;
         }
 
+        if (this.isString(exp)) {
+            return exp.slice(1, -1);
+        }
+
         throw 'Unimplemented';
     }
 
     isNumber(exp) {
         return typeof exp === 'number';
+    }
+
+    isString(exp) {
+        return (
+            typeof exp === 'string' && exp[0] === '"' && exp.slice(-1) === '"'
+        );
     }
 }
 
@@ -20,5 +30,6 @@ class Eva {
 const eva = new Eva();
 
 assert.strictEqual(eva.eval(1), 1);
+assert.strictEqual(eva.eval('"hello"'), 'hello');
 
 console.log('All assertions passed!');
